ecbe6acf835b8c56c1e363534078270c2180c7e8
Abhi Mehrotra <abhimehrotra@bears.mybrcc.edu>
2024-12-20 19:08:44 -0600

    Add test structure

---
+++ tests/data_processing/test_processor.py
@@ -0,0 +1,5 @@
+import pytest
+from src.data_processing.data_processor import DataProcessor
+
+def test_data_processor():
+    pass  # Add your tests here
---
+++ tests/visualization/test_visualization.py
@@ -0,0 +1,5 @@
+import pytest
+from src.visualization.visualization import Visualizer
+
+def test_visualizer():
+    pass  # Add your tests here
